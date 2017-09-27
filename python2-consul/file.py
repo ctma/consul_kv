a74f7edff1155f8de8d4f9cba8cc02ff95bc6c21
Charles Ma <macharlest@gmail.com>
2017-09-27 13:00:12 -0700

    Refactor file handling into its own class

---
+++ python2-consul/file.py
@@ -0,0 +1,131 @@
+import os
+import yaml
+import fnmatch
+import logging
+
+class File():
+
+    def __init__(self, file_handler=None):
+        self.file_handler = file_handler
+
+    def is_directory(self, user_input):
+        '''
+        Check to see if the input is a file
+        args:
+            user_input: str
+        return
+            boolean
+        '''
+        return os.path.isdir(user_input)
+
+    def is_file(self, user_input):
+        '''
+        Check to see if the input is a directory
+        args:
+            user_input: str
+        return
+            boolean
+        '''
+        return os.path.isfile(user_input)
+
+    def read_file_content(self, file):
+        '''
+        Attempts to open the file and return the content
+        args:
+            file:
+        return
+            str
+        '''
+        logging.info("Attempting to read content from file: {}".format(file))
+        file_handler = None
+        try:
+            file_handler = open(file)
+        except FileNotFoundError:
+            logging.error("File {} does not exist")
+            exit(1)
+        content = file_handler.read()
+        file_handler.close()
+        return content
+
+    def is_yaml_file(self, file_content):
+        '''
+        Check to see if the content of the file is valid yaml
+        args:
+            file_content: str
+        return
+            boolean
+        '''
+        yaml_file = None
+        try:
+            yaml_file = yaml.load(file_content)
+        except yaml.scanner.ScannerError:
+            logging.error("File {} contains invalid yaml".format(file_content))
+        return (True if yaml_file else False)
+    
+    def parse_yaml(self, file_content):
+        '''
+        Parse the yaml file and return a json if valid otherwise None
+        args:
+            file_content: str
+        return:
+            json
+        '''
+        return (yaml.load(file_content) if self.is_yaml_file(file_content) else None)
+
+    def glob_yaml_file(self, user_input):
+        '''
+        If file_handler is a directory, scan for all *yaml and return a list of file
+        args:
+            user_input: str
+        return
+            list of str
+        '''
+
+        # Credit:
+        # https://stackoverflow.com/questions/2186525/use-a-glob-to-find-files-recursively-in-python
+        files = []
+        for root, dirnames, filenames in os.walk(user_input):
+            for filename in fnmatch.filter(filenames, '*.yaml'):
+                files.append(os.path.join(root, filename))
+        # End Credit
+        return files
+
+    def process_file(self, file):
+        '''
+        Process the file content and return a list
+        args:
+            file: dict
+        return
+            list of dict
+        '''
+        yaml_data = []
+        logging.debug("Processing content from file: {}".format(file))
+        file_content = self.read_file_content(file)
+        if file_content:
+            logging.debug("Parsing yaml content from file {}".format(file))
+            data = self.parse_yaml(file_content)
+            if data:
+                logging.debug("YAML in file {} is: \n{}".format(file,data))
+                yaml_data.append(data)
+            else:
+                logging.error("File {} does not contain valid yaml".format(file))
+        return yaml_data
+
+    def process_directory(self, directory):
+        '''
+        Process each file and load it into a list of dict
+        args:
+            directory: str
+        return
+            list of json
+        '''
+        yaml_data = []
+        files = self.glob_yaml_file(directory)
+        if files:
+            for file in files:
+                yaml_data += self.process_file(file)
+            #logging.debug("Valid yaml data: {}".format(yaml_data))
+        else:
+            logging.info("Directory {} contains no file with extension .yaml".format(directory))
+        return yaml_data
+
--- python2-consul/update_consul.py
+++ python2-consul/update_consul.py
@@ -1,21 +1,20 @@
 import argparse
-import fnmatch
 import re
-import os
-import yaml
 import validators
 import logging
+from file import File
 from consul import ConsulOperation
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("python update_consul.py -f blah.yaml")
-    parser.add_argument("-f", "--file", help="Read from a particular yaml file")
-    parser.add_argument("-d", "--directory", help="Provide the directory to glob for *.yml")
+    parser.add_argument("-f", "--file", help="Provide the directory path or file path",
+                        required=True)
     parser.add_argument("-r", "--retry", help="Provide the number of retry you wish to perform",
                         default=2)
     parser.add_argument("-u", "--url", help="Provide the consul url including the port.",
                         default="http://127.0.0.1:8500")
     parser.add_argument("-t", "--token", help="Provide the consul token", default="")
+    parser.add_argument("-p", "--port", help="Provide the consul port", default=8500)
     parser.add_argument("-l", "--loglevel",
                         help="Provide the log level: INFO, DEBUG, ERROR, WARNING, CRITICAL",
                         default="INFO")
@@ -24,46 +23,23 @@ if __name__ == "__main__":
     if validators.url(args.url):
         syntax = re.compile(':\d+$')
         if not syntax.search(args.url):
-            args.url = args.url + ':8500'
+            args.url = args.url + ':' + str(args.port)
     else:
-        logging.error("{} is an invalid url".format(args.url))
+        print("{} is an invalid url".format(args.url))
         exit(1)
     consul = ConsulOperation(args.url, args.token, args.retry)
-    yaml_data = None
     payload = None
-    if args.file is None and args.directory is None:
-        logging.error("Exiting... Missing file or directory argument. Use '-h' to view options")
+    if args.file is None:
+        print("Exiting... Missing file or directory argument. Use '-h' to view options")
         exit(1)
-    # Pass the file object to a method and let it handle the processing
-    # These last two statements is not DRY. Need to rewrite it
     elif args.file:
-        try:
-            yaml_file = open(args.file)
-        except IOError:
-            logging.error("File {} does not exist or path is invalid".format(args.file))
-            exit(1)
-        yaml_data = yaml.load(yaml_file)
-        payload = consul.parse_yaml(yaml_data)
-        logging.debug("Consul payload: {}".format(payload))
+        if File().is_directory(args.file):
+            data_set = File().process_directory(args.file)
+            payload = []
+            for data in data_set:
+                logging.debug("Extracting kv: {}".format(data))
+                payload += consul.parse_yaml(data)
+        elif File().is_file(args.file):
+            data_set = File().process_file(args.file)
         consul.submit_transaction(payload)
         consul.validate_kv(payload)
-    elif args.directory:
-        # Credit:
-        # https://stackoverflow.com/questions/2186525/use-a-glob-to-find-files-recursively-in-python
-        files = []
-        for root, dirnames, filenames in os.walk(args.directory):
-            for filename in fnmatch.filter(filenames, '*.yaml'):
-                files.append(os.path.join(root, filename))
-        # End Credit
-        for file in files:
-            try:
-                yaml_file = open(file)
-            except IOError:
-                logging.error("Skipping... File {} is not a valid yaml".format(file))
-                continue
-            yaml_data = yaml.load(yaml_file)
-            logging.info("Processing {}".format(file))
-            payload = consul.parse_yaml(yaml_data)
-            logging.debug("Consul payload: {}".format(payload))
-            consul.submit_transaction(payload)
-            consul.validate_kv(payload)
