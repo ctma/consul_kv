c75ee6a0ee2f542463b5ca8cb81b06a6a6650d4c
Charles Ma <macharlest@gmail.com>
2017-10-03 10:02:10 -0700

    Add initial setup file

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='python2-consul',
+    packages=['python2-consul'],
+    version='0.0.1',
+    install_requires=[
+        'certifi==2017.4.17',
+        'chardet==3.0.4',
+        'idna==2.5',
+        'PyYAML==3.12',
+        'requests==2.18.1',
+        'urllib3==1.21.1',
+        'validators==0.12.0',
+        'pytest==3.2.2'
+    ]
+)
